44c8625391f6b7aceb0bb8fbe7ea7bf841c22291
Viktor Topcheev <ViktorTopcheev@yandex.by>
2020-12-03 19:50:58 +0300

    newcomment

--- Number5_1.java
+++ Number5_1.java
@@ -1,10 +1,19 @@
-
+import java.util.Objects;
+import java.util.Scanner;
 public class Number5_1 {
     public static void main(String[] arg) {
-        public boolean equals(Object obj) {
-
-            return (this == obj);
+        Scanner scanner = new Scanner(System.in);
+        String name =  scanner.nextLine();
+        if(Objects.equals(name, "Вася")){
+            System.out.println("Привет! \n Я тебя так долго ждал");
         }
+        if(Objects.equals(name, "Анастасия")){
+            System.out.println("Я тебя так долго ждал");
+        }
+        if(!Objects.equals(name, "Вася") && Objects.equals(name, "Анастасия")){
+
+            System.out.println("Добрый день, а вы кто?");
+            }
 
 
 
